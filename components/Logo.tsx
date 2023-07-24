625f01e8a6d03cf4bcf23e1da2dbb6640402f645
flaming-streamz <flamingstreamz@gmail.com>
2023-07-24 11:09:17 +0300

    added: logo navigates to home when clicked

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface Props {}
 
 const Logo: React.FunctionComponent<Props> = ({}) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/");
+  };
+
   return (
     <Image
       src="/images/logo.png"
@@ -12,6 +19,7 @@ const Logo: React.FunctionComponent<Props> = ({}) => {
       height="100"
       className="hidden cursor-pointer
   md:block"
+      onClick={handleClick}
     />
   );
 };
