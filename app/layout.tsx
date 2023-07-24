0959fbe5c74e2e33dae62ea3fb519aadfc21ec97
flaming-streamz <flamingstreamz@gmail.com>
2023-07-24 12:50:04 +0300

    wrapped navbar in clientcomp-only

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 
-import { MainNavbar } from "@/components";
+import { ClientComponentOnly, MainNavbar } from "@/components";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -15,7 +15,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={font.className}>
-        <MainNavbar />
+        <ClientComponentOnly>
+          <MainNavbar />
+        </ClientComponentOnly>
         {children}
       </body>
     </html>
